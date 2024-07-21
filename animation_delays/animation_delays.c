0603f019d0045b3cb000a169a025e9bdb137f2ec
csnv <jorgecsnova@gmail.com>
2024-07-21 18:29:44 +0200

    Updated skill animation delays up until rebirth Added animation break on hit feature

--- animation_delays/animation_delays.c
+++ animation_delays/animation_delays.c
@@ -3,7 +3,7 @@
 //===== By: ========================================================
 //= csnv
 //===== Version: ===================================================
-//= 0.1alpha
+//= 1.1
 //===== Description: ===============================================
 //= Enables hard delays based on an aproximation of client behavior
 //===== Repository: ================================================
@@ -33,6 +33,7 @@
 
 /**
  * Allow skills to bypass animation delays
+ * (Recommended)
  * 
  * Comment/uncomment to toggle the bypass behavior.
  * Only skills with the bypass setting defined as true are able to bypass animation delays.
@@ -42,6 +43,7 @@
 
 /**
  * Allow breaking animation delays with the player movement. Also called skill dancing
+ * (Recommended)
  *
  * Comment/uncomment to toggle the movement/dancing behavior.
  * Client reproduces the walking animation when forced to move, breaking the animation of
@@ -50,10 +52,26 @@
  */
 #define ANIM_DELAY_ALLOW_DANCING
 
+/**
+ * Allow breaking animation by getting hit
+ * (Not recommended)
+ * VERIFY: Not reproducible in recent clients
+ * 
+ * Comment/uncomment to toggle the hit/break animation behavior
+ * Client reproduces the flinch animation when getting hit, breaking the previous skill
+ * animation and allowing the player to cast a skill
+ * Users can "exploit" this behavior by intentionally hitting each other with rapid-low damage multihit skills
+ */
+//#define ANIM_BREAK_ON_HIT
+
+#ifdef ANIM_BREAK_ON_HIT
+	#define MULTIHIT_INTERVAL 200
+#endif
+
 HPExport struct hplugin_info pinfo = {
 	"animation_delays",   // Plugin name
 	SERVER_TYPE_MAP,             // Which server types this plugin works with?
-	"0.1",                       // Plugin version
+	"1.0",                       // Plugin version
 	HPM_VERSION,                 // HPM Version (don't change, macro is automatically updated)
 };
 
@@ -78,6 +96,9 @@ struct skill_adelay_timer {
 	int x;
 	int y;
 #endif
+#ifdef ANIM_BREAK_ON_HIT
+	time_t hit_tick;
+#endif
 };
 
 /**
@@ -94,6 +115,9 @@ static struct skill_adelay_timer* get_adelays_timer(struct map_session_data* sd)
 #ifdef ANIM_DELAY_ALLOW_DANCING
 		data->x = -1;
 		data->y = -1;
+#endif
+#ifdef ANIM_BREAK_ON_HIT
+		data->hit_tick = 0;
 #endif
 		addToMSD(sd, data, 0, true);
 	}
@@ -234,21 +258,26 @@ static void read_skill_animation_delays(bool minimal)
  */
 static int post_skill_not_ok(int retVal, uint16 skill_id, struct map_session_data* sd)
 {
-	if (retVal == 1) {
+	if (retVal == 1)
 		// It's already not ok to perform this skill
 		return 1;
-	}
 
 	struct skill_adelay_timer* data = get_adelays_timer(sd);
 
-	if (data->last_skill_id == 0) {
-		return retVal;
+	if (data->last_skill_id == 0)
+		return 0;
+
+#ifdef ANIM_BREAK_ON_HIT
+	if (data->hit_tick > data->tick) {
+		// User got hit after last skill, breaking the animation
+		return 0;
 	}
+#endif
+
 #ifdef ANIM_DELAY_ALLOW_DANCING
-	if (data->x != sd->bl.x || data->y != sd->bl.y) {
+	if (data->x != sd->bl.x || data->y != sd->bl.y)
 		// User is moving or skill-dancing
-		return retVal;
-	}
+		return 0;
 #endif
 
 	int last_skill_index = skill->get_index(data->last_skill_id);
@@ -256,42 +285,51 @@ static int post_skill_not_ok(int retVal, uint16 skill_id, struct map_session_dat
 	struct skill_adelay_entry *last_skill_adelay = skill_adelay_list[last_skill_index];
 	struct skill_adelay_entry *current_skill_adelay = skill_adelay_list[current_skill_index];
 
-	if (!last_skill_adelay || !current_skill_adelay) {
+	if (!last_skill_adelay || !current_skill_adelay)
 		return 0;
-	}
 
 #ifdef ANIM_DELAY_ALLOW_BYPASS
-	if (current_skill_adelay->bypass) {
+	if (current_skill_adelay->bypass)
 		// Skill ignores any delay
 		return 0;
-	}
 #endif
 
-	uint32 sd_adelay = status_get_adelay(&sd->bl);
-	int delay = last_skill_adelay->delay;
+	int delay = last_skill_adelay->delay; // Direct delay
 
-	if (data->last_skill_id != skill_id && last_skill_adelay->delay_others != 0) {
+	if (data->last_skill_id != skill_id && last_skill_adelay->delay_others != 0) // Delay if it's a different skill
 		delay = last_skill_adelay->delay_others;
-	}
 
-	if (delay < 0) { // Delay is a percentage over character's adelay
-		delay = abs(delay) * sd_adelay / 100;
-	}
+	if (delay < 0) // Delay is a percentage over character's adelay
+		delay = abs(delay) * status_get_adelay(&sd->bl) / 100;
 
-	if (last_skill_adelay->max_delay != 0 && delay > last_skill_adelay->max_delay) {
+	if (last_skill_adelay->max_delay != 0 && delay > last_skill_adelay->max_delay) // Delay should not exceed MaxDelay
 		delay = last_skill_adelay->max_delay;
-	}
 
 	int64 tick = timer->gettick();
 	int64 tick_diff = tick - data->tick;
 
-	if (tick_diff < (delay * ANIM_DELAY_LENIENCY / 100)) {
+	if (tick_diff < (delay * ANIM_DELAY_LENIENCY / 100))
 		// Last skill delay still in place
 		return 1;
-	}
 
 	return 0;
 }
+
+/**
+ * Update last_hit_tick from player
+ */
+#ifdef ANIM_BREAK_ON_HIT
+static int update_last_hit_timer(int tid, int64 tick, int id, intptr_t data)
+{
+	struct map_session_data* sd = map->id2sd(id);
+	if (sd == NULL)
+		return 0;
+
+	struct skill_adelay_timer* skill_data = get_adelays_timer(sd);
+	skill_data->hit_tick = tick;
+	return 1;
+}
+#endif
 
 /****************************/
 /* Hooks */
@@ -304,7 +342,6 @@ static int post_clif_skill_nodamage(int retVal, struct block_list* src, struct b
 static void post_clif_skill_poseffect(struct block_list* src, uint16 skill_id, int val, int x, int y, int64 tick)
 {
 	switch (skill_id) {
-	// Avoid skills that use poseffect timely
 	case WZ_METEOR:
 		break;
 	default:
@@ -323,8 +360,30 @@ static int post_clif_skill_damage(int retVal, struct block_list* src, struct blo
 			set_adelays_timer(src, skill_id);
 	}
 
+#ifdef ANIM_BREAK_ON_HIT
+	if (dst != NULL && dst->type == BL_PC && in_damage > 0) {
+		for (int i = 0; i < div; i++) {
+			timer->add(tick + sdelay + (i * MULTIHIT_INTERVAL), update_last_hit_timer, dst->id, 0);
+		}
+	}
+#endif
+
 	return retVal;
 }
+
+#ifdef ANIM_BREAK_ON_HIT
+static int post_clif_damage(int retVal, struct block_list* src, struct block_list* dst, int sdelay, int ddelay, int64 in_damage, short div, enum battle_dmg_type type, int64 in_damage2)
+{
+	if (dst != NULL && dst->type == BL_PC && in_damage > 0) {
+		for (int i = 0; i < div; i++) {
+			timer->add(timer->gettick() + sdelay + (i * MULTIHIT_INTERVAL), update_last_hit_timer, dst->id, 0);
+		}
+	}
+	return retVal;
+}
+#endif
+
+
 
 // Plugin initialization
 HPExport void plugin_init(void)
@@ -337,4 +396,7 @@ HPExport void plugin_init(void)
 	addHookPost(clif, skill_nodamage, post_clif_skill_nodamage);
 	addHookPost(clif, skill_poseffect, post_clif_skill_poseffect);
 	addHookPost(clif, skill_damage, post_clif_skill_damage);
+#ifdef ANIM_BREAK_ON_HIT
+	addHookPost(clif, damage, post_clif_damage);
+#endif
 }
